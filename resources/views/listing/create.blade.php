be501f9a33029629b46a05db1d7a3a3ef2c81b91
kasldfh <forumf41l@gmail.com>
2015-12-09 17:08:51 -0600

    updated views to reflect batch

--- resources/views/listing/create.blade.php
+++ resources/views/listing/create.blade.php
@@ -5,17 +5,35 @@
 <label for="quantity">Quantity</label>
 <input name="quantity"></input>
 <br>
+<br>
+
+<label for="price">Price</label>
+<input name="price"></input>
+<br>
+<br>
+
+<label for="batch_id">Price</label>
+<input name="batch_id"></input>
+<br>
+<br>
 
 <label for="useby">Use By date</label>
 <input name="useby" type="date"></input>
 <br>
+<br>
+
+<label for="date_produced">Date Produced</label>
+<input name="date_produced" type="date"></input>
+<br>
 
 <label for="type">Type</label>
 <select name="type">
-  <option>Sap</option>
-  <option>Syrup</option>
+@foreach ($types as $type)
+  <option value="{!!$type->type_id!!}">{!!$type->type_name!!}</option>
+@endforeach
 </select>
 
+<br>
 <br>
 <input type="submit" class="btn btn-primary"></input>
 
