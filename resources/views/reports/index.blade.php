76561ca896ea7fedb1860349d8958314b033d055
hobbman <plzrememberthisemail@gmail.com>
2015-12-12 17:58:03 -0600

    added names to elements

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -2,14 +2,14 @@
 @section('content')
 <p>Select the type of report you would like to generate:</p>
 <div class="panel">
-	<form>
-		<select>
+	<form method="GET" action="/reports">
+		<select name="type">
 			<option value="selling">Sells</option>
 			<option value="product">Product</option>
 			<option value="buying">Buying</option>
 		</select>
-		<input type="text" id="datepicker1">
-		<input type="text" id="datepicker2">
+		<input type="text" id="datepicker1" name="start_date">
+		<input type="text" id="datepicker2" name="end_date">
 		<br><br>
 		<button type="button" style="margin-left:10%;">Generate Report</button>
 	</form>
